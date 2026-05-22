6b879ed7e7886e8f4c4920a53b891a05863af02d
Joao Astolfi <jpastolfi@gmail.com>
2026-05-22 17:33:31 +0100

    Python module 01 Ex 0 && 1

---
+++ milestone_02/python_module_01/ex0/ft_garden_intro.py
@@ -0,0 +1,11 @@
+def ft_garden_intro():
+    print("""=== Welcome to My Garden ===
+Plant: Rose
+Height: 25cm
+Age: 30 days
+          
+=== End of Program ===""")
+
+
+if __name__ == "__main__":
+    ft_garden_intro()
