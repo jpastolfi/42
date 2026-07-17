340eeeaa755f392ad3e7cde85fc8b12f325be089
Joao Pedro Astolfi Da Costa <jastolfi@c2r3s8.42lisboa.com>
2026-07-17 14:00:43 +0100

    Python Module 7 ex0

--- milestone_02/python_module_09/ex0/space_station.py
+++ milestone_02/python_module_09/ex0/space_station.py
@@ -1,20 +1,55 @@
-from pydantic import BaseModel, Field
+from uuid import uuid4
+from pydantic import BaseModel, Field, ValidationError
+from datetime import date, datetime
 
 
 class SpaceStation(BaseModel):
-    station_id: str = Field(min_length=3, max_length=10)  # 3-10 characters
-    name: str = Field(min_length=1, max_length=50)  # 1-50 characters
-    crew_size: int = Field(ge=1, le=20)  # 1-20 people
-    power_level: float = Field(ge=0.0, le=100.0)  # 0.0-100.0 percent
-    oxygen_level: float = Field(ge=0.0, le=100.0)  # 0.0-100.0 percent
-    last_maintenance: str  # DateTime field
+    station_id: str = Field(min_length=3, max_length=10)
+    name: str = Field(min_length=1, max_length=50)
+    crew_size: int = Field(ge=1, le=20)
+    power_level: float = Field(ge=0.0, le=100.0)
+    oxygen_level: float = Field(ge=0.0, le=100.0)
+    last_maintenance: datetime
     is_operational: bool = True
-    notes: str = Field(max_length=200)  # Optional string max 200 characters
+    notes: str | None = Field(default=None, max_length=200)
 
 
 def main():
-    teste = SpaceStation(station_id="Jpapa")
-    print(teste)
+    try:
+        valid_spacestation = SpaceStation(
+            station_id=uuid4().hex[:8],
+            name="Death Star",
+            crew_size=18,
+            power_level=25.0,
+            oxygen_level=50.0,
+            last_maintenance=date(2023, 3, 1),
+        )
+        print("Space Station Data Validation")
+        print("=============================================")
+        print("Valid station created:")
+        for item in valid_spacestation.model_dump().items():
+            key, value = item
+            print(f"{key.capitalize()}: {value}")
+        print("=============================================")
+
+        invalid_spacestation = SpaceStation(
+            station_id=uuid4().hex[:8],
+            name="",
+            crew_size=365,
+            power_level=25.0,
+            oxygen_level=50.0,
+            last_maintenance=date(2023, 3, 1),
+        )
+        print("Space Station Data Validation")
+        print("=============================================")
+        print("Valid station created:")
+        for item in invalid_spacestation.model_dump().items():
+            key, value = item
+            print(f"{key.capitalize()}: {value}")
+        print("=============================================")
+    except ValidationError as e:
+        for item in e.errors():
+            print(f"Error in field '{item['loc'][0]}': {item['msg']}")
 
 
 if __name__ == "__main__":
